d5329104b8b78ceb3caed6f7a155a88b9dc34d99
kon28289 <kon28289@naver.com>
2025-10-08 16:31:38 +0900

    first commit

---
+++ src/main/java/com/gpt/geumpumtabackend/GeumpumtaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gpt.geumpumtabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeumpumtaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeumpumtaBackendApplication.class, args);
+    }
+
+}
