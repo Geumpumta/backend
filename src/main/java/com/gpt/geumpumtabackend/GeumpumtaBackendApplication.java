68db86d525801df6b2f0d202431aa0f5979f9bc0
kon28289 <kon28289@naver.com>
2025-10-23 14:48:38 +0900

    feat: 감사 어노테이션 추가

--- src/main/java/com/gpt/geumpumtabackend/GeumpumtaBackendApplication.java
+++ src/main/java/com/gpt/geumpumtabackend/GeumpumtaBackendApplication.java
@@ -1,9 +1,16 @@
 package com.gpt.geumpumtabackend;
 
+import com.gpt.geumpumtabackend.global.oauth.user.KakaoProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@ConfigurationPropertiesScan
+@EnableConfigurationProperties({KakaoProperties.class})
 public class GeumpumtaBackendApplication {
 
     public static void main(String[] args) {
