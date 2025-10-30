2579e48341fc5e9d66bbbeeea9449cc032a9b3e5
Juhye0k <kyoung1678@naver.com>
2025-10-30 13:09:41 +0900

    fix : 유효성 검증 누락 추가

--- src/main/java/com/gpt/geumpumtabackend/study/dto/request/StudyStartRequest.java
+++ src/main/java/com/gpt/geumpumtabackend/study/dto/request/StudyStartRequest.java
@@ -1,9 +1,13 @@
 package com.gpt.geumpumtabackend.study.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDateTime;
 
 
-public record StudyStartRequest(LocalDateTime startTime){
+public record StudyStartRequest(
+        @NotNull(message = "startTime은 필수입니다")
+        LocalDateTime startTime){
 
 }
 
