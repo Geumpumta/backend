78f5cbd721532b6dc7aee801f5ff4be82b7dcae5
kon28289 <kon28289@naver.com>
2025-10-30 19:20:24 +0900

    feat: 메일 인증코드 발송 및 검증 api 추가

---
+++ src/main/java/com/gpt/geumpumtabackend/user/controller/EmailController.java
@@ -0,0 +1,59 @@
+package com.gpt.geumpumtabackend.user.controller;
+
+import com.gpt.geumpumtabackend.global.aop.AssignUserId;
+import com.gpt.geumpumtabackend.global.response.ResponseBody;
+import com.gpt.geumpumtabackend.global.response.ResponseUtil;
+import com.gpt.geumpumtabackend.user.dto.request.EmailCodeRequest;
+import com.gpt.geumpumtabackend.user.dto.request.EmailCodeVerifyRequest;
+import com.gpt.geumpumtabackend.user.dto.response.EmailCodeVerifyResponse;
+import com.gpt.geumpumtabackend.user.service.EmailService;
+import com.gpt.geumpumtabackend.user.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/email")
+@RequiredArgsConstructor
+public class EmailController {
+
+    private final EmailService emailService;
+    private final UserService userService;
+
+    /**
+     * 이메일 인증 코드 전송 api
+     * @param request
+     * @return
+     */
+    @GetMapping("/request-code")
+    @AssignUserId
+    @PreAuthorize("isAuthenticated() and hasRole('GUEST')")
+    public ResponseEntity<ResponseBody<Void>> requestCode(
+            @RequestBody @Valid EmailCodeRequest request,
+            Long userId
+    ){
+        userService.saveSchoolEmail(userId, request);
+        emailService.sendMail(request);
+        return ResponseEntity.ok(ResponseUtil.createSuccessResponse());
+    }
+
+    /**
+     * 이메일 인증 코드 검증 api
+     * @param request
+     * @return
+     */
+    @GetMapping("/verify-code")
+    @AssignUserId
+    @PreAuthorize("isAuthenticated() and hasRole('GUEST')")
+    public ResponseEntity<ResponseBody<EmailCodeVerifyResponse>> verifyCode(
+            @RequestBody EmailCodeVerifyRequest request,
+            Long userId
+    ){
+        return ResponseEntity.ok(ResponseUtil.createSuccessResponse(emailService.verifyCode(request, userId)));
+    }
+}
