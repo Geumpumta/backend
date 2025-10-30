7b8cb510547ae01eb16e56317290ee7d417c1bdd
kon28289 <kon28289@naver.com>
2025-10-30 19:20:06 +0900

    feat: 학교 이메일 검증 api 추가

--- src/main/java/com/gpt/geumpumtabackend/user/controller/UserController.java
+++ src/main/java/com/gpt/geumpumtabackend/user/controller/UserController.java
@@ -1,12 +1,13 @@
 package com.gpt.geumpumtabackend.user.controller;
 
+
 import com.gpt.geumpumtabackend.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
 public class UserController {
 
--- src/main/java/com/gpt/geumpumtabackend/user/service/UserService.java
+++ src/main/java/com/gpt/geumpumtabackend/user/service/UserService.java
@@ -5,6 +5,7 @@ import com.gpt.geumpumtabackend.global.exception.BusinessException;
 import com.gpt.geumpumtabackend.global.exception.ExceptionType;
 import com.gpt.geumpumtabackend.user.domain.User;
 import com.gpt.geumpumtabackend.user.domain.UserRole;
+import com.gpt.geumpumtabackend.user.dto.request.EmailCodeRequest;
 import com.gpt.geumpumtabackend.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -22,5 +23,12 @@ public class UserService {
                 .orElseThrow(()->new BusinessException(ExceptionType.USER_NOT_FOUND));
 
         return user.getRole().equals(UserRole.ADMIN);
+    }
+
+    @Transactional
+    public void saveSchoolEmail(Long userId, EmailCodeRequest request) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(()->new BusinessException(ExceptionType.USER_NOT_FOUND));
+        user.setSchoolEmail(request.email());
     }
 }
