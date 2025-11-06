d71911a69e114fe6dee9f8e867ff841845c9cf14
kon28289 <kon28289@naver.com>
2025-11-06 17:30:19 +0900

    feat: 학번 학부 필드 및 추가 정보, 닉네임 설정 메서드 추가

--- src/main/java/com/gpt/geumpumtabackend/user/domain/User.java
+++ src/main/java/com/gpt/geumpumtabackend/user/domain/User.java
@@ -3,6 +3,7 @@ package com.gpt.geumpumtabackend.user.domain;
 
 import com.gpt.geumpumtabackend.global.base.BaseEntity;
 import com.gpt.geumpumtabackend.global.oauth.user.OAuth2Provider;
+import com.gpt.geumpumtabackend.user.dto.request.CompleteRegistrationRequest;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
@@ -40,18 +41,32 @@ public class User extends BaseEntity {
 
     private String providerId;
 
+    @Column(unique = true)
+    private String studentId;
+
+    @Enumerated(value = EnumType.STRING)
+    private Department department;
+
     @Builder
-    public User(String email, UserRole role, String name, String picture, OAuth2Provider provider, String providerId) {
+    public User(String email, UserRole role, String name, String picture, OAuth2Provider provider, String providerId, Department department) {
         this.email = email;
         this.role = role;
         this.name = name;
         this.picture = picture;
         this.provider = provider;
         this.providerId = providerId;
+        this.department = department;
     }
 
-    public void registerSchoolEmail(String schoolEmail) {
-        this.schoolEmail = schoolEmail;
+    public void completeRegistration(CompleteRegistrationRequest request) {
+        this.schoolEmail = request.email();
+        this.studentId = request.studentId();
+        this.department = Department.from(request.department());
+        this.role = UserRole.USER;
+    }
+
+    public void setInitialNickname(String nickname) {
+        this.nickname = nickname;
     }
 
 }
