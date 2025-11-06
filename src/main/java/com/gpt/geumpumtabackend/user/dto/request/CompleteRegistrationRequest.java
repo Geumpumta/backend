ad6ada0e01f6dd6d6b534215264409d721a4eb00
kon28289 <kon28289@naver.com>
2025-11-06 19:29:12 +0900

    feat: dto 필수 필드 검증 코드 추가

--- src/main/java/com/gpt/geumpumtabackend/user/dto/request/CompleteRegistrationRequest.java
+++ src/main/java/com/gpt/geumpumtabackend/user/dto/request/CompleteRegistrationRequest.java
@@ -1,11 +1,14 @@
 package com.gpt.geumpumtabackend.user.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 public record CompleteRegistrationRequest(
         @Pattern(regexp = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@kumoh\\.ac\\.kr$", message = "학교 이메일을 입력하세요")
         String email,
+        @NotBlank(message = "학번을 입력하세요.")
         String studentId,
+        @NotBlank(message = "학부를 입력하세요.")
         String department
 ) {
 
