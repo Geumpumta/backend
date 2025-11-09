d6fabbcd07b73306590c1f60613bf716138a65d7
Juhye0k <kyoung1678@naver.com>
2025-11-09 11:36:17 +0900

    fix : DepartmentRankingTemp 파라미터 타입 String 수정

--- src/main/java/com/gpt/geumpumtabackend/rank/dto/DepartmentRankingTemp.java
+++ src/main/java/com/gpt/geumpumtabackend/rank/dto/DepartmentRankingTemp.java
@@ -9,8 +9,9 @@ public class DepartmentRankingTemp {
     private Long totalMillis;
     private Long ranking;
 
-    public DepartmentRankingTemp(String departmentName, Long totalMillis, Long ranking) {
-        this.departmentName = departmentName;
+    // JPA 쿼리에서 Department enum을 받기 위한 추가 생성자
+    public DepartmentRankingTemp(Department department, Long totalMillis, Long ranking) {
+        this.departmentName = department.name(); // enum을 String으로 변환
         this.totalMillis = totalMillis;
         this.ranking = ranking;
     }
