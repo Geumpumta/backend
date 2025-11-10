cc7837d38c7e5475ac07f040ea39618eb1af9cb9
kon28289 <kon28289@naver.com>
2025-11-10 14:00:54 +0900

    feat: redirectUrl 화이트 리스트 추가

--- src/main/java/com/gpt/geumpumtabackend/global/oauth/util/RedirectUrlValidator.java
+++ src/main/java/com/gpt/geumpumtabackend/global/oauth/util/RedirectUrlValidator.java
@@ -7,7 +7,8 @@ public class RedirectUrlValidator {
     private static final Set<String> ALLOWED_PREFIXES = Set.of(
             "http://localhost:5173",
             "http://localhost:8080",
-            "exp://"
+            "exp://",
+            "geumpumta://"
 
     );
 
