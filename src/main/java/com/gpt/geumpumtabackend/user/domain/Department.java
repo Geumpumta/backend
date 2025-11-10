47630716a458d82cd04a1c3331bfda08b8af10b0
kon28289 <kon28289@naver.com>
2025-11-10 17:35:24 +0900

    chore: 학과 enum 변경

--- src/main/java/com/gpt/geumpumtabackend/user/domain/Department.java
+++ src/main/java/com/gpt/geumpumtabackend/user/domain/Department.java
@@ -9,19 +9,30 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum Department {
 
-    ARCH_CIV_ENV("건축토목환경공학부", "School of Architecture, Civil and Environmental Engineering"),
-    MECHANICAL("기계공학부", "School of Mechanical Engineering"),
-    INDUSTRIAL_BIGDATA("산업·빅데이터공학부", "School of Industrial and Big Data Engineering"),
-    MATERIALS("재료공학부", "School of Materials Engineering"),
-    ELECTRICAL_ELECTRONIC("전자공학부", "School of Electrical and Electronic Engineering"),
-    COMPUTER("컴퓨터공학부", "School of Computer Engineering"),
-    CHEMICAL_MATERIALS("화학소재공학부", "School of Chemical and Materials Engineering"),
-    GENERAL("자율전공학부", "School of General Studies"),
-    BUSINESS("경영학과", "Department of Business Administration"),
-    LIBERAL("교양학부", "School of Liberal Arts"),
-    OPTICAL("광시스템공학과", "Department of Optical Systems Engineering"),
-    BIOMEDICAL("바이오메디컬공학과", "Department of Biomedical Engineering"),
-    IT_CONVERGENCE("IT융합학과", "Department of IT Convergence");
+    architectureEngineering("건축공학전공", "Architecture Engineering"),
+    architecture("건축학전공", "Architecture"),
+    civilEngineering("토목공학전공", "Civil Engineering"),
+    environmentalEngineering("환경공학전공", "Environmental Engineering"),
+    mechanicalEngineering("기계공학전공", "Mechanical Engineering"),
+    mechanicalSystemsEngineering("기계시스템공학전공", "Mechanical Systems Engineering"),
+    smartMobility("스마트모빌리티공학전공", "Smart Mobility Engineering"),
+    industrialEngineering("산업공학전공", "Industrial Engineering"),
+    appliedMathBigData("수리빅데이터공학전공", "Applied Mathematics and Big Data Engineering"),
+    polymerEngineering("고분자공학전공", "Polymer Engineering"),
+    materialsEngineering("신소재공학전공", "Materials Engineering"),
+    semiconductorSystems("반도체시스템전공", "Semiconductor Systems Engineering"),
+    electronicSystems("전자시스템공학전공", "Electronic Systems Engineering"),
+    software("소프트웨어전공", "Software Engineering"),
+    artificialIntelligence("인공지능공학전공", "Artificial Intelligence Engineering"),
+    computerEngineering("컴퓨터공학전공", "Computer Engineering"),
+    materialsDesignEngineering("소재디자인공학전공", "Materials Design Engineering"),
+    chemicalEngineering("화학공학전공", "Chemical Engineering"),
+    chemicalBioMaterials("화학생명공학전공", "Chemical and Biomaterials Engineering"),
+    opticalSystems("광시스템공학전공", "Optical Systems Engineering"),
+    biomedicalEngineering("바이오메디컬공학전공", "Biomedical Engineering"),
+    itConvergence("IT융합학전공", "IT Convergence"),
+    liberalMajor("자율전공학부", "Liberal Major"),
+    businessAdministration("경영학과", "Business Administration");
 
     private final String koreanName;
     private final String englishName;
