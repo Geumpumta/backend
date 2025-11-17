5854fc9be958a4c514be8c8a741bbdf5eef61bfb
kon28289 <kon28289@naver.com>
2025-11-17 22:38:48 +0900

    feat: 닉네임 검증 및 프로필 수정 dto 추가

---
+++ src/main/java/com/gpt/geumpumtabackend/user/dto/request/ProfileUpdateRequest.java
@@ -0,0 +1,11 @@
+package com.gpt.geumpumtabackend.user.dto.request;
+
+import jakarta.validation.constraints.Pattern;
+
+public record ProfileUpdateRequest(
+        String ImageUrl,
+        String publicId,
+        @Pattern(regexp = "^[가-힣a-zA-Z0-9]{2,11}$", message = "닉네임은 한글, 영어, 숫자로 2~11자 이내여야 합니다.")
+        String nickname
+) {
+}
