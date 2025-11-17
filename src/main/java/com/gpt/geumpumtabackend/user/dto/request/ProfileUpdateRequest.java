316a62abad9f18efed6967510d25cef26eaa322f
kon28289 <kon28289@naver.com>
2025-11-17 22:49:33 +0900

    chore: 닉네임 정규식 수정

--- src/main/java/com/gpt/geumpumtabackend/user/dto/request/ProfileUpdateRequest.java
+++ src/main/java/com/gpt/geumpumtabackend/user/dto/request/ProfileUpdateRequest.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.Pattern;
 public record ProfileUpdateRequest(
         String ImageUrl,
         String publicId,
-        @Pattern(regexp = "^[가-힣a-zA-Z0-9]{2,11}$", message = "닉네임은 한글, 영어, 숫자로 2~11자 이내여야 합니다.")
+        @Pattern(regexp = "^[가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z0-9]{2,11}$", message = "닉네임은 한글, 영어, 숫자로 2~11자 이내여야 합니다.")
         String nickname
 ) {
 }
