7e02051acf631cf4bd37beb274d19a3908edc7a8
Juhye0k <kyoung1678@naver.com>
2025-11-18 17:58:13 +0900

    refactor : 개인랭킹 프로필 이미지, 학과 데이터 추가

--- src/main/java/com/gpt/geumpumtabackend/rank/dto/response/PersonalRankingEntryResponse.java
+++ src/main/java/com/gpt/geumpumtabackend/rank/dto/response/PersonalRankingEntryResponse.java
@@ -3,19 +3,22 @@ package com.gpt.geumpumtabackend.rank.dto.response;
 
 import com.gpt.geumpumtabackend.rank.dto.PersonalRankingTemp;
 
-//TODO : 학과 추가
 public record PersonalRankingEntryResponse(
         Long userId,
         Long totalMillis,
         Long rank,
-        String username
+        String username,
+        String imageUrl,
+        String department
 ) {
     public static PersonalRankingEntryResponse of(PersonalRankingTemp userRankingTemp) {
         return new PersonalRankingEntryResponse(
                 userRankingTemp.getUserId(),
                 userRankingTemp.getTotalMillis(),
                 userRankingTemp.getRanking(),
-                userRankingTemp.getUsername()
+                userRankingTemp.getUsername(),
+                userRankingTemp.getImageUrl(),
+                userRankingTemp.getDepartment()
         );
     }
 
