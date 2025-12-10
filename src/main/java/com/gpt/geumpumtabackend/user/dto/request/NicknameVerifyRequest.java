16632f6913dd9f380ac500eabd67a6ea67c9c020
kon28289 <kon28289@naver.com>
2025-12-10 11:58:25 +0900

    chore: 닉네임에 공백을 허용하도록 정규식 수정

--- src/main/java/com/gpt/geumpumtabackend/user/dto/request/NicknameVerifyRequest.java
+++ src/main/java/com/gpt/geumpumtabackend/user/dto/request/NicknameVerifyRequest.java
@@ -3,7 +3,9 @@ package com.gpt.geumpumtabackend.user.dto.request;
 import jakarta.validation.constraints.Pattern;
 
 public record NicknameVerifyRequest (
-        @Pattern(regexp = "^[가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z0-9]{2,11}$", message = "닉네임은 한글, 영어, 숫자로 2~11자 이내여야 합니다.")
-        String nickname
+        @Pattern(
+                regexp = "^(?! )[가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z0-9 ]{2,11}(?<! )$",
+                message = "닉네임은 한글, 영어, 숫자, 공백 포함 2~11자 이내여야 합니다."
+        )        String nickname
 ){
 }
