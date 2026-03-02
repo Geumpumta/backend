9d70db072dd2e1d241629b07bbee2c6b9102fe29
kon28289 <kon28289@naver.com>
2026-03-02 15:22:26 +0900

    feat: Maintenance 도메인 상태 관리 로직 추가

---
+++ src/main/java/com/gpt/geumpumtabackend/maintenance/repository/MaintenanceRepository.java
@@ -0,0 +1,7 @@
+package com.gpt.geumpumtabackend.maintenance.repository;
+
+import com.gpt.geumpumtabackend.maintenance.domain.Maintenance;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MaintenanceRepository extends JpaRepository<Maintenance, Long> {
+}
